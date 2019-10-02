d2a923225b47bd415f206f4d27e7454f0de9bcc0
Paul Cross <pcross@usgs.gov>
2019-10-02 06:47:47 -0600

    vignette not working, deleted for now

---
+++ R/CWDsims.r
@@ -0,0 +1,14 @@
+#' CWDsims: A package for simulating chronic wasting disease models.
+#'
+#' Description here
+#' 
+#' @section Plotting functions:
+#' The plotting functions ...
+#'
+#' @section model functions
+#' 
+#' @section helper functions
+#' 
+#' @docType package
+#' @name CWDsims
+NULL
