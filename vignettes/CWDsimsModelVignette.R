1a31e61336f18c4a1117247532db2f3cc86be389
Cross, Paul C <pcross@usgs.gov>
2019-10-23 09:14:22 -0600

    replacing the vignettes

---
+++ vignettes/CWDsimsModelVignette.R
@@ -0,0 +1,11 @@
+## ----setup, include=FALSE, message=FALSE------------------
+library(knitr)
+library(CWDsims)
+options(continue=" ")
+options(width=60)
+knitr::opts_chunk$set(echo = TRUE,
+                      warning = FALSE,
+                      message = FALSE,
+                      fig.height = 7,
+                      fig.width = 7)
+
